10c9b27868b2e15404878d4ef38d42fa81c6824a
hyper-glitch <graff049@gmail.com>
2022-08-11 19:02:26 +0300

    feature(stream): add function that stream archive bytes

--- server.py
+++ server.py
@@ -1,9 +1,51 @@
-from aiohttp import web
+import asyncio
+import datetime
+from asyncio import subprocess
+
 import aiofiles
+from aiohttp import web
+
+INTERVAL_SECS = 1
+
+
+async def uptime_handler(request):
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
 
 
 async def archive(request):
-    raise NotImplementedError
+    response = web.StreamResponse()
+    response.headers['Content-Disposition'] = 'attachment; filename="photos.zip"'
+    program = 'zip'
+    args = ['-', 'test_photos', '-r']
+    bytes_portion = 100000
+    archive_hash = request.match_info.get('archive_hash', 'unknown')
+
+    await response.prepare(request)
+
+    content = b''
+    process = await asyncio.create_subprocess_exec(program, *args, stdout=subprocess.PIPE)
+
+    while not process.stdout.at_eof():
+        content = await process.stdout.read(n=bytes_portion)
+        await response.write(content)
+
+    return response
 
 
 async def handle_index_page(request):
