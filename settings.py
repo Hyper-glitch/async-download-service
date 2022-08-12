53dacb7828edd4779f4b89649325a13a0b1857be
hyper-glitch <graff049@gmail.com>
2022-08-12 17:08:22 +0300

    feature(settings): add module for setting

---
+++ settings.py
@@ -0,0 +1,11 @@
+"""Module for setting vars."""
+from environs import Env
+
+env = Env()
+env.read_env()
+
+WITHOUT_DELAY = 0
+ENABLE_RESPONSE_DELAY = env.bool('ENABLE_RESPONSE_DELAY', False)
+ENABLE_LOGGING = env.bool('ENABLE_LOGGING', False)
+RESPONSE_DELAY = env.int('RESPONSE_DELAY', 0)
+PHOTOS_DIR = env.str('PHOTOS_DIR', 'test_photos')
