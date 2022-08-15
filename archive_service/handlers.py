5e1a1711e1cd020788cef7a4d4bcea3c0999be37
hyper-glitch <graff049@gmail.com>
2022-08-15 10:56:02 +0300

    fix(docs): make docstrings like Google Python Style Guide

--- archive_service/handlers.py
+++ archive_service/handlers.py
@@ -1,21 +1,45 @@
-"""Module for handlers."""
+"""Module for microservice handlers."""
 import os
 
 import aiofiles
 from aiohttp import web
+from aiohttp.abc import Request, StreamResponse
 
 
-async def handle_cwd_exists(request, cwd):
+async def handle_cwd_exists(request: Request, cwd: str) -> None:
+    """Check that archive exists or not.
+
+    Args:
+        request: request from client that trigger this endpoint.
+        cwd: current work directory.
+    Raises:
+     HTTPNotFound: if the archive does not exist or has been deleted.
+    """
     if not os.path.exists(cwd):
-        raise web.HTTPNotFound(text='Архив не существует или был удален.')
+        raise web.HTTPNotFound(text='The archive does not exist or has been deleted.')
 
 
-async def handle_cwd_name(request, archive_hash):
+async def handle_cwd_name(request: Request, archive_hash: str) -> None:
+    """Check the name of dir, that will be archived.
+
+    Args:
+        request: request from client that trigger this endpoint.
+        archive_hash: name of archive hash.
+    Raises:
+        HTTPNotImplemented: because can't archive directory with name '.' or '..'
+    """
     if archive_hash == '.' or archive_hash == '..':
         raise web.HTTPNotImplemented(text='Архив с катологом "." или ".." не может быть создан.')
 
 
-async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r') as index_file:
+async def handle_index_page(request: Request) -> StreamResponse:
+    """Open and return response with html content.
+
+    Args:
+        request: request from client that trigger this endpoint.
+    Returns:
+        response: response with content from a server.
+    """
+    async with aiofiles.open('archive_service/index.html', mode='r') as index_file:
         index_contents = await index_file.read()
     return web.Response(text=index_contents, content_type='text/html')
